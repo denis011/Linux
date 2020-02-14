4266e1bd526510524cd4ee7e4f142fd818e3cf05
denis <denis011@gmail.com>
2020-02-14 22:03:30 +0100

    izmena mount.sh

--- mount.sh
+++ mount.sh
@@ -3,11 +3,11 @@
 
 echo Start mounting Windows shares
 
-sudo mount -t cifs -o credentials=/home/denis/kredencijali //pc/Filmovi /mnt/filmovi/
-sudo mount -t cifs -o credentials=/home/denis/kredencijali //pc/Muzika /mnt/muzika/
-sudo mount -t cifs -o credentials=/home/denis/kredencijali //pc/Downloads /mnt/downloads/
-sudo mount -t cifs -o credentials=/home/denis/kredencijali //pc/Install /mnt/install/
-sudo mount -t cifs -o credentials=/home/denis/kredencijali //pc/My\ Documents /mnt/documents/
+sudo mount -t cifs -o credentials=/home/denis/Linux/kredencijali //pc/Filmovi /mnt/filmovi/
+sudo mount -t cifs -o credentials=/home/denis/Linux/kredencijali //pc/Muzika /mnt/muzika/
+sudo mount -t cifs -o credentials=/home/denis/Linux/kredencijali //pc/Downloads /mnt/downloads/
+sudo mount -t cifs -o credentials=/home/denis/Linux/kredencijali //pc/Install /mnt/install/
+sudo mount -t cifs -o credentials=/home/denis/Linux/kredencijali //pc/My\ Documents /mnt/documents/
 
 echo Finished mounting Windows shares
 
